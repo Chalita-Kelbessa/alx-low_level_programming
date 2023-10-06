b25d88b9f573343ce12407d536a2c11ea3731caf
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-05 19:19:57 -0700

    taks 5 crr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Retun: 0 when successful
+ * Return: Always 0 when successfull
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
