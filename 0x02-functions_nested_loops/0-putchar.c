3d91a5376c929750849070573a78829d8c2ed5d9
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-10 11:46:50 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-echo _putchar
+#include <stdio.h>
+
+/**
+ * main - program prints putchar
+ *
+ * Return: 0 Success
+ *
+ */
+
+int main(void)
+{
+	char str[15] = "_putchar";
+
+	printf("String: ");
+
+	printf("\"%s\"", str);
+
+	return (0);
+}
