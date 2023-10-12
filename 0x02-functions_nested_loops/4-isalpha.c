37633c8144e6a62ce06321469095d415c6b75661
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-12 14:08:03 -0700

    tak 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,11 +10,10 @@
 
 int _isalpha(int c)
 {
-for (c = 97; c <= 122; c++)
-	return (1);
-for (c = 0; c < 97; c++)
-	return (0);
-for (c = 127; c < 127; c--)
-	return (0);
-return (0);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (0);
+	else
+		return (1);
+
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of  the day
+ *
+ * Return 0
+ *
+ */
+
+void jack_bauer(void)
+{
+	a = 58;
+	b = 58;
+	c = 58;
+	d = 58;
+
+	while (a <= 50)
+	{
+		b = 48;
+		while (b <= 54)
+		_putchar (a);
+		_putchar (b);
+	_putchar ('\n');
+	return (0);
+}
