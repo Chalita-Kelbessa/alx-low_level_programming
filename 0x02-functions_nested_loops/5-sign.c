10b1e48ed4040a8ff1f0ea0032efa421cafd1046
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-12 14:54:09 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,26 +4,26 @@
  * print_sign - To putch sign of a number
  *
  * @n: any number
- *
+ * Return: 0 or 1
  */
 
 int print_sign(int n)
 {
 while ((n) > 0)
 	{
-	return (1);
+	return (43 && 49);
 	_putchar(43);
 	n++;
 	}
 while ((n) < 0)
 	{
-	return (-1);
+	return (45 && 49);
 	_putchar(45);
 	n--;
 	}
 while (n == 0)
 	{
-	return (0);
+	return (48 && 48);
 	_putchar(48);
 	}
 return (0);
