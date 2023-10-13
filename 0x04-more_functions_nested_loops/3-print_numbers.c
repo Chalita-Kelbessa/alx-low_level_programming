e8e175bf8b06c8e10b0683a22c9089b5e2ff9513
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-13 15:17:44 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - outputs numbers 0 to 9
+ *
+ * Return: void success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+		_putchar(i);	
+}
