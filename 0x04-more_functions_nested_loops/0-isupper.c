e0ea2964bf290193f4e7486f589cc964e5b0e328
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-13 13:41:13 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - checks for upper case character
+ *
+ * Return: 1 success
+ *
+ */
+
+int _is upper(int c)
+{
+	if (c >= 65 && c <= 90)
+
+		return (1);
+
+	else
+	
+		return (0);
+}
