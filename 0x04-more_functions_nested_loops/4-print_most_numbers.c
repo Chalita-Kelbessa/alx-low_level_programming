7f51c79e4a5171d56b3887cf0c5cbe9d958963fc
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-13 16:20:41 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 selectively
+ *
+ * Return: ; success 0
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 47; i <= 57; i++)
+		if (i == 2 && i == 4)
+		_putchar(!i);
+	else
+		_putchar(i);
+}
