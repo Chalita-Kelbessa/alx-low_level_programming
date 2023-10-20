62dcba2260629d25f4d3801a3039435046a60c0e
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-19 19:07:42 -0700

    9 task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,18 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i; 
+	int j; 
+	int n;
 
 	for (i = 48; i <= 57; i++)
 	{
 		for (j = 48; j <= 57; j++)
 		{
-			_putchar(i * j);
+			n = i * j;
+			_putchar(n);
+			_putchar(44);
+			_putchar(9);
 		}
 		_putchar('\n');
 	}
