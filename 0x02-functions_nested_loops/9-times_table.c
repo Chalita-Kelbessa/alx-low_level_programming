eab39a666b1bf9e5d2bc0855dce2f0db03978adc
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-19 19:48:06 -0700

    9 task6

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,19 +18,22 @@ void times_table(void)
 		continue;
 		for (j = 0; j <= 9; j++)
 		{
+			if (j != 0)
+			{
 			n = i * j;
 			_putchar(44);
 			_putchar(32);
 
-			if (n <= 9)
-			{
-				_putchar(32);
-				_putchar(n + 48);
-			}
-			else
-			{
-				_putchar((n / 10) + 48);
-				_putchar((n % 10) + 48);
+				if (n <= 9)
+				{
+					_putchar(32);
+					_putchar(n + 48);
+				}
+				else
+				{
+					_putchar((n / 10) + 48);
+					_putchar((n % 10) + 48);
+				}
 			}
 		}
 		_putchar('\n');
