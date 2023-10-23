eab659c0a0f9ab118262fa77ccfab34af223ec3c
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-23 14:28:03 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,15 +10,6 @@
 
 int add(int a, int b)
 {
-	if (b == 0)
-	{
-	return (a);
 
-		if (a < 0 && b < 0)
-		{
-			return ((0 - 1) * a + (0 - 1) * b);
-		}
-	}
-	else
-		return (1 + add(a, b - 1));
+	return (a + b);
 }
