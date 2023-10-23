45681e1e576c503405c34b91ab6f9fdd199006d6
Chalita-Kelbessa <chalurga@gmail.com>
2023-10-23 13:26:18 -0700

    rask 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,6 +19,8 @@ void times_table(void)
 		for (j = 1; j <= 9; j++)
 		{
 			n = i * j;
+			_putchar(44);
+			_putchar(32);
 				if (n <= 9)
 				{
 					_putchar(32);
@@ -29,11 +31,6 @@ void times_table(void)
 					_putchar((n / 10) + 48);
 					_putchar((n % 10) + 48);
 				}
-			if (j < 9)
-			{
-				_putchar(44);
-				_putchar(32);
-			}
 		}
 
 		_putchar('\n');
