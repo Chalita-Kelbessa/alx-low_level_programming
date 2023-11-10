696e1bd70c913f38a76605355891f95421001bc3
Chalita-Kelbessa <chalurga@gmail.com>
2023-11-10 14:58:21 -0800

    task 0 crr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,14 +16,16 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	int i;
 
-	arr - malloc(sizeof(char) * size);
+	ar - malloc(sizeof(char) * size);
 
-	if (arr == NULL)
+	if (ar == NULL)
 		return (NULL);
 
 	if (size == 0)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
-		arr[i] = c;
-	return (arr);
+		ar[i] = c;
+
+	return (ar);
 }
